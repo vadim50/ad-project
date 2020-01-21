eaa6a591da73e884c4ae045eb8e22d05554e66a4
vadim50 <waswas4@yandex,ru>
2020-01-21 23:51:21 +0300

    Добавление ссылок и адаптация

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,126 @@
 <template>
-    <v-card
-    color="grey lighten-4"
-    flat
-    height="200px"
-    tile
-  >
-    <v-navigation-drawer app temporary></v-navigation-drawer>
-      <v-toolbar dark color="primary">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-item link
+        v-for="link of links"
+        :key="link.title"
+        :href="link.url"
+        >
+          <v-list-item-action>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ link.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+<!--         <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item> -->
+      </v-list>
+    </v-navigation-drawer>
 
-      <v-toolbar-title>Add Aplication</v-toolbar-title>
-
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="hidden-md-and-up"/>
+      <v-toolbar-title>Application</v-toolbar-title>
       <v-spacer></v-spacer>
-        <v-toolbar-items class="hidden-sm-and-down">
-          <v-btn depressed large>Link One</v-btn>
-          <v-btn depressed large>Link Two</v-btn>
-          <v-btn depressed large>Link Three</v-btn>
-        </v-toolbar-items>
-    </v-toolbar>
+      <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn depressed
+        color="primary"
+        x-large
+        v-for="link in links"
+        :key="link.title"
+        v-bind:href="link.url"
+        >
+        <v-icon class="material-icons" left>{{ link.icon }}</v-icon>
+        {{ link.title }}
+      </v-btn>
+    </v-toolbar-items>
+    </v-app-bar>
+
     <v-content>
-     <!--  <router-view></router-view> -->
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col class="text-center">
+            <v-tooltip left>
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  :href="source"
+                  icon
+                  large
+                  target="_blank"
+                  v-on="on"
+                >
+                  <v-icon large>mdi-code-tags</v-icon>
+                </v-btn>
+              </template>
+              <span>Source</span>
+            </v-tooltip>
+
+            <v-tooltip right>
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  icon
+                  large
+                  href="https://codepen.io/johnjleider/pen/zgxeLQ"
+                  target="_blank"
+                  v-on="on"
+                >
+                  <v-icon large>mdi-codepen</v-icon>
+                </v-btn>
+              </template>
+              <span>Codepen</span>
+            </v-tooltip>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-content>
-  </v-card>
+    <v-footer
+      color="primary"
+      app
+    >
+      <span class="white--text">&copy; {{ new Date().getFullYear() }} mastertoy50@gmail.com</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld';
-
-export default {
-  
-};
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+      links: [
+        {title: 'Login',       icon: 'lock',            url: '/login'},
+        {title: 'Registation', icon: 'face',            url: '/registration'},
+        {title: 'Orders',      icon: 'bookmark_border', url: '/orders'},
+        {title: 'New ad',      icon: 'bug_report',         url: '/new'},
+        {title: 'My ads',      icon: 'list',            url: '/list'},
+      ],
+    }),
+  }
 </script>
+<style>
+  a.v-btn, a.v-list-item{
+    text-decoration: none;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 
 
+
 Vue.config.productionTip = false
+// Vue.use(vuetify, {
+//   iconfont: 'mdi' // 'md' || 'mdi' || 'fa' || 'fa4'
+// })
 
 
 new Vue({
